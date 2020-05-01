20d578e108a86986d247586806d0927c49efd521
zahidyew <zahidyew96@gmail.com>
2020-05-01 12:53:31 +0800

    add whitelist & url query

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const request = require('request');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  let whitelisted = ['http://127.0.0.1:5500', 'https://zahidyew.github.io'];
+  let origin = req.headers.origin;
+
+  if (whitelisted.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   next();
 });
 
-app.get('/jokes/random', (req, res) => {
+///bypass?url=https://joke-api-strict-cors.appspot.com/jokes/random
+///bypass?url=PUT-API-URL-HERE
+app.get('/bypass', (req, res) => {
   request(
-    { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    { url: req.query.url },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
